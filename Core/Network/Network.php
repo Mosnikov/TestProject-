85d3d59fee2c19826d3faacf42d916426466fdc5
Mosnikov <moshnikov.m@gmail.com>
2020-09-28 17:58:55 +0300

    Revert "lhljhlk"
    This reverts commit 274f2dcec3cb67166faa5ae71e21a8aba9c01494.

--- Core/Network/Network.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined("INDEX_ACCESS")) exit("Нельзя запустить скрипт: " . __FILE__);
-
-class Network {
-
-}
--- Screen/ContentController/ContentController.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined("INDEX_ACCESS")) exit("Нельзя запустить скрипт: " . __FILE__);
-require_once('Screen/Base/BaseController.php');
-
-class ContentController extends BaseController
-
-    function generateHTML() {
-
-}
