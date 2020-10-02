9e7b8cc1665500e06b455ac17f2a0aa996d07d83
Mosnikov <moshnikov.m@gmail.com>
2020-10-02 18:07:14 +0300

    create my Controller

---
+++ yii2.test/controllers/admin/MyController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace app\controllers\admin;
+
+use yii\web\Controller;
+
+class MyController extends Controller {
+
+    public function actionIndex (){
+
+        return __METHOD__;
+    }
+
+
+}
